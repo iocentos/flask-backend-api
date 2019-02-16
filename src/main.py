9c768253dcebe704902c974c9ea6649502f24e5c
Ioannis Sintos <io@iocentos.com>
2019-02-16 22:01:59 +0200

    Start on hello world flask server

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print ('hello')
+from flask import Flask
+
+
+# Create main application object
+app = Flask(__name__)
+
+
+
+# Routes registration
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/users")
+def users():
+    return "Hello users!"
+
+
+# End of routes registration
+
+
+
+# Main app code
+if __name__ == "__main__":
+    app.run()
