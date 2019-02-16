01706381ba47c4061c0af55b3a56f58042488e50
Ioannis Sintos <io@iocentos.com>
2019-02-16 22:16:59 +0200

    Create base storage object

---
+++ src/storage/storage.py
@@ -0,0 +1,17 @@
+# Returns user from backend storage
+def get_user(user_id):
+    return {
+            "id": 0,
+            "name": "John"
+            }
+
+# Stores a new group in the backend storage and returns group id
+def create_group(group_name):
+    return {
+            "id": 0,
+            "name": "tomato"
+            }
+
+# Stores a new message in the backend storage
+def store_message(group_id, user_id, message):
+    print("Storing message")
